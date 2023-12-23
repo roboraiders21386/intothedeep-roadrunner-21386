ad5a11791dc00cc1dd5bc0b474f705b0d65036b7
amjadj <amjad.jabs@gmail.com>
2023-12-22 21:22:44 -0600

    Upgrade 1.10 and FTC Wires latest release merged.
    Test Robot Wiz with Drive Encoders caliberation copied

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MecanumDrive.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MecanumDrive.java
@@ -104,7 +104,7 @@ public final class MecanumDrive {
         public double axialVelGain = 0.0;
         public double lateralVelGain = 0.0;
         public double headingVelGain = 0.0; // shared with turn
-        //TODO End Step 12
+        //TODO End Step 13
     }
 
     public static Params PARAMS = new Params();

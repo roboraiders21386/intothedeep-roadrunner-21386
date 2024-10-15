2cbf268c53967d30d0921ed57204e06e0fe3f70d
RoboRaidersAA <aryamane.aditi@gmail.com>
2024-10-15 15:42:56 -0500

    PLS TEST THIS AHHHHHHHHH

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TELEOP/TeleOpimtrying.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TELEOP/TeleOpimtrying.java
@@ -1,79 +1,102 @@
 package org.firstinspires.ftc.teamcode.TELEOP;
-
+//libraries are a collection of resources
 import com.acmerobotics.roadrunner.Pose2d;
 import com.acmerobotics.roadrunner.PoseVelocity2d;
 import com.acmerobotics.roadrunner.Vector2d;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.CRServo;
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.Servo;
 
 import org.firstinspires.ftc.teamcode.subsystems.MecanumDrive;
 import org.firstinspires.ftc.teamcode.subsystems.TankDrive;
 import org.firstinspires.ftc.teamcode.tuning.TuningOpModes;
 
-
-@TeleOp(name = "Tx-Rx TeleOp", group = "00-Teleop")
-public class FTCWiresTeleOpMode extends LinearOpMode {
-    private Servo intake;
-    private Servo arm;
+@TeleOp(name = "the best teleop clearly", group = "00-Teleop")
+public class TeleOpimtrying extends LinearOpMode{
+//what are these controls bro ToT
+    //declaring a variable giving a name to a device
     private DcMotor lift;
+    private CRServo Intake;
+    private Servo Extension;
+    private Servo Rotation;
+    private Servo Claw;
+    //declaring a variable - giving a name to a value
+    private double dp = 0.5;
+    private double intPow = 0.5;
+    private double liftPow = 0.25;
     @Override
     public void runOpMode() throws InterruptedException {
         double SLOW_DOWN_FACTOR = 0.5; //TODO Adjust to driver comfort
         double LOW_BASKET_LIFT = 1000; //TODO Make sure this is accruate when we have game elements
+        //helps you debug code by sending a message to the driver hub
         telemetry.addData("Initializing FTC Wires (ftcwires.org) TeleOp adopted for Team:","21386");
         telemetry.update();
 
-        //initializing
+        //initializing your devices connecting your name to the configuration you inputted into the driver hub
         lift = hardwareMap.get(DcMotor.class, "lift");
-        //intake = hardwareMap.get(Servo.class, "INTAKE"); JUST IN CASE
-        //arm = hardwareMap.get(Servo.class, "ARM");
+        Intake = hardwareMap.get(CRServo.class, "intake");
+        Extension = hardwareMap.get(Servo.class, "extend");
+        Rotation = hardwareMap.get(Servo.class, "rotate");//is this the thing that rotates backwards
+        Claw = hardwareMap.get(Servo.class, "claw"); //Specimen Claw
+        //setting a direction for your motor and initializing the encoder - a device that tracks how far your robot is going using ticks
+        lift.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        lift.setDirection(DcMotorSimple.Direction.FORWARD);
 
+        //if you are using a mecanum drive this is what the program will use
         if (TuningOpModes.DRIVE_CLASS.equals(MecanumDrive.class)) {
             MecanumDrive drive = new MecanumDrive(hardwareMap, new Pose2d(0, 0, 0));
 
             waitForStart();
-
+            //FINALLY getting into the driving part
             while (opModeIsActive()) {
-                telemetry.addData("Running FTC Wires (ftcwires.org) TeleOp Mode adopted for Team:","TEAM NUMBER");
+                telemetry.addData("Running FTC Wires (ftcwires.org) TeleOp Mode adopted for Team:","21386");
                 drive.setDrivePowers(new PoseVelocity2d(
                         new Vector2d(
-                                -gamepad1.left_stick_y * SLOW_DOWN_FACTOR,
+                                //forward backward
+                                -gamepad1.left_stick_y * SLOW_DOWN_FACTOR,//var that gives you more control over your driving
+                                //strafing
                                 -gamepad1.left_stick_x * SLOW_DOWN_FACTOR
                         ),
+                        //turning
                         -gamepad1.right_stick_x * SLOW_DOWN_FACTOR
-                ));
+                )); //this is more concise drivetrain :D
 
                 drive.updatePoseEstimate();
 
-                //telemetry.addData("LF Encoder", drive.leftFront.getCurrentPosition());
-                //telemetry.addData("LB Encoder", drive.leftBack.getCurrentPosition());
-                //telemetry.addData("RF Encoder", drive.rightFront.getCurrentPosition());
-                //telemetry.addData("RB Encoder", drive.rightBack.getCurrentPosition());
+                //all the encoder stuff relaying the robots position to the driver hub
+                telemetry.addData("LF Encoder", drive.leftFront.getCurrentPosition());
+                telemetry.addData("LB Encoder", drive.leftBack.getCurrentPosition());
+                telemetry.addData("RF Encoder", drive.rightFront.getCurrentPosition());
+                telemetry.addData("RB Encoder", drive.rightBack.getCurrentPosition());
 
+                //gives you a number stating where the robot is and where it's going
                 telemetry.addLine("Current Pose");
                 telemetry.addData("x", drive.pose.position.x);
                 telemetry.addData("y", drive.pose.position.y);
                 telemetry.addData("heading", Math.toDegrees(drive.pose.heading.log()));
                 telemetry.update();
             }
+            //referencing the tuning files if you are using a tank drive
         } else if (TuningOpModes.DRIVE_CLASS.equals(TankDrive.class)) {
             TankDrive drive = new TankDrive(hardwareMap, new Pose2d(0, 0, 0));
 
             waitForStart();
-
+            //remember this is tank drive!!!!
             while (opModeIsActive()) {
                 drive.setDrivePowers(new PoseVelocity2d(
                         new Vector2d(
                                 -gamepad1.left_stick_y * SLOW_DOWN_FACTOR,
-                                0.0
+                                0.0 //no strafing :(
                         ),
                         -gamepad1.right_stick_x * SLOW_DOWN_FACTOR
                 ));
 
                 drive.updatePoseEstimate();
 
+                //the encoder for tank drive
                 telemetry.addData("x", drive.pose.position.x);
                 telemetry.addData("y", drive.pose.position.y);
                 telemetry.addData("heading", drive.pose.heading);
@@ -82,6 +105,7 @@ public class FTCWiresTeleOpMode extends LinearOpMode {
         } else {
             throw new AssertionError();
         }
+        //ps5 code
         if (gamepad1.circle){
             lift.setTargetPosition(0); //set to start position
             telemetry.addData("Set to", "start value");
@@ -93,8 +117,8 @@ public class FTCWiresTeleOpMode extends LinearOpMode {
             telemetry.update();
         }
         if (gamepad1.cross){
-            lift.setTargetPosition(700);//set to rigging/specimin position
-            telemetry.addData("Set to", "specimin value");
+            lift.setTargetPosition(700);//set to rigging/specimen position
+            telemetry.addData("Set to", "specimen value");
             telemetry.update();
         }
         if (gamepad1.square){
@@ -111,6 +135,18 @@ public class FTCWiresTeleOpMode extends LinearOpMode {
             lift.setTargetPosition(lift.getCurrentPosition()-50);
             telemetry.addData("Decrementing by", "50");
             telemetry.update();
+        }
+        if (gamepad1.left_bumper){
+            Intake.setPower(-1);
+        }
+        if (gamepad1.right_bumper){
+            Intake.setPower(1);
+        }
+        if (gamepad1.left_trigger>0){
+            Claw.setPosition(0);
+        }
+        if (gamepad1.right_trigger>0){
+            Claw.setPosition(1);
         }
     }
 }
